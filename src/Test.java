48bfd57f457530d955d91ec62f9dea5b72adb428
Harsha Kokel <harshakokel@gmail.com>
2018-04-24 09:36:41 -0500

    threshold 35 runs

--- src/Test.java
+++ src/Test.java
@@ -2,12 +2,12 @@
 public class Test {
 
 	static String[][] datasets = {
-//			{"accidents.test.data","accidents.ts.data", "accidents.valid.data"}, 
-//			{"baudio.test.data", "baudio.ts.data", "baudio.valid.data"}, 
+			{"accidents.test.data","accidents.ts.data", "accidents.valid.data"}, 
+			{"baudio.test.data", "baudio.ts.data", "baudio.valid.data"}, 
 //			{"bnetflix.test.data","bnetflix.ts.data","bnetflix.valid.data"}, 
 //			{"dna.test.data","dna.ts.data","dna.valid.data"},  
-//			{"jester.test.data", "jester.ts.data","jester.valid.data"},  
-			{"kdd.test.data", "kdd.ts.data","kdd.valid.data"},  
+			{"jester.test.data", "jester.ts.data","jester.valid.data"},  
+//			{"kdd.test.data", "kdd.ts.data","kdd.valid.data"},  
 //			{"msnbc.test.data", "msnbc.ts.data","msnbc.valid.data"},  
 //			{"nltcs.test.data", "nltcs.ts.data", "nltcs.valid.data"}, 
 //			{"plants.test.data", "plants.ts.data", "plants.valid.data"},  
@@ -16,7 +16,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		String[] testArgs, validationArgs;
-		int[] kValues = {5};
+		int[] kValues = {20};
 		int numberOfIterations = 10;
 		double bestLogLikelihood = Double.NEGATIVE_INFINITY;
 		for(String[] dataset : datasets) {
@@ -48,11 +48,11 @@ public class Test {
 				prevLogLikelihood = avgLogLikelihood;
 			}
 			long startTime = System.currentTimeMillis();
-			System.out.println(" Best BN is "+ bestKBN.sizeOfLatentVariable);
+//			System.out.println(" Best BN is "+ bestKBN.sizeOfLatentVariable);
 //			bestKBN.testLogLikelihood = 0.0;
 //			bestKBN.numberOfTestSamples	= 0;
 //			bestKBN.processData("datasets/"+dataset[0], false);
-			System.out.println("K value: "+ bestKBN.sizeOfLatentVariable +" | "+dataset[0]+" : "+(bestKBN.testLogLikelihood/bestKBN.numberOfTestSamples));
+//			System.out.println("K value: "+ bestKBN.sizeOfLatentVariable +" | "+dataset[0]+" : "+(bestKBN.testLogLikelihood/bestKBN.numberOfTestSamples));
 			long endTime   = System.currentTimeMillis();
 			System.out.println("time: "+ (endTime - startTime)/1000);
 		}
